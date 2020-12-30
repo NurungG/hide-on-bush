50efb8e6f77cc54f6a1054fd2fad68f644c3c6b8
Jinwook <2inuc.bae@gmail.com>
2020-12-30 16:48:20 +0900

    Main page

--- popup.js
+++ popup.js
@@ -1,15 +1,5 @@
-let changeColor = document.getElementById('changeColor');
+var toggle_switch = $("input[type='checkbox']");
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-}
+toggle_switch.click(function() {
+    $("p").toggle();
+})
