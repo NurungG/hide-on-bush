bf8042d02016d92e14b894bd225211ac06cb13fb
Jinwook <2inuc.bae@gmail.com>
2020-12-30 18:57:55 +0900

    Add layout of secondary page

--- popup.js
+++ popup.js
@@ -2,4 +2,9 @@ $("#goto_blocked_keywords_list").click(function() {
     document.body.innerHTML = "";
 
     $('body').animate({height:"300px"});
+
+    setTimeout(function() {
+        $('body').load("list.html");
+    }, 300);
+
 });
