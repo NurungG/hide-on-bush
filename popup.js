202ef1d8d35b5771cbd234093e0bb4709f5951da
Jinwook <2inuc.bae@gmail.com>
2020-12-31 19:38:03 +0900

    Design new plate

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
-$("#goto_blocked_keywords_list").click(function() {
-    document.body.innerHTML = "";
+// onoff toggle button click event
+document.getElementById("onoff-input").onclick = function(element) {
+    let onoff_btn = document.getElementById("onoff-btn");
+    console.log(onoff_btn);
+    if (onoff_btn.classList.contains("btn-off")) {
+        onoff_btn.classList.remove("btn-off");
+        onoff_btn.classList.add("btn-on");
 
-    $('body').animate({height:"300px"});
+        onoff_btn.getElementsByClassName("toggle-text")[0].innerHTML = "ON";
+    } else {
+        onoff_btn.classList.remove("btn-on");
+        onoff_btn.classList.add("btn-off");
 
-    setTimeout(function() {
-        $('body').load("list.html");
-    }, 300);
+        onoff_btn.getElementsByClassName("toggle-text")[0].innerHTML = "OFF";
+    }
+};
 
-});
+// footer tab click event
+let footer_tab = document.getElementsByClassName("footer-tab");
+console.log(footer_tab);
+for (var i = 0; i < footer_tab.length; i++) {
+    footer_tab[i].onclick = function() {
+        /* TODO: load tab page */
+    }
+}
