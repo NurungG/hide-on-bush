61f0797b0cb0dbf24764aeeaf7f074c425b4de6b
Jinwook Bae <2inuc.bae@gmail.com>
2021-01-07 18:02:08 +0900

    Edit initial keywordList

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
     /* Init the extension */
     chrome.storage.sync.set({'onoff':false});
-    chrome.storage.sync.set({'keywordList':['hi', 'hello', 'jinwook bae', 'hwichance', 'hideonbush', 'text blind']});
+    chrome.storage.sync.set({'keywordList':['Hi!', 'This is Hide-on-bush', 'We love Faker']});
 });
 
-let cnt = 0;
 chrome.webNavigation.onCompleted.addListener(function(e) {
     chrome.storage.sync.get(null, function(d) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
