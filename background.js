234ffea335071baf9a7541e5b68c4270444d1fe0
Jinwook Bae <2inuc.bae@gmail.com>
2021-01-27 15:02:51 +0900

    Fix not to query current window

--- background.js
+++ background.js
@@ -27,11 +27,11 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.onClicked.addListener(contextMenusListener);
 });
 
-chrome.webNavigation.onCompleted.addListener(function(e) {
-    chrome.storage.sync.get(null, function(d) { // null: get all data
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {'op': 'page-load', 'data': d});
-        });
+chrome.webNavigation.onCompleted.addListener(function(event) {
+    let tabId = event.tabId;
+    chrome.storage.sync.get(null, function(data) {
+        chrome.tabs.sendMessage(tabId, {'op': 'page-load', 'data': data});
+        console.log('page load msg sent to ' + tabId);
     });
 });
 
