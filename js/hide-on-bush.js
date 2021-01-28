e61a22b6ee8b33fb3b7b1532df03afe8c452d51d
Jinwook Bae <2inuc.bae@gmail.com>
2021-01-28 14:35:46 +0900

    Remove old functions

--- js/hide-on-bush.js
+++ js/hide-on-bush.js
@@ -3,23 +3,3 @@ document.addEventListener('DOMContentLoaded', function(e) {
         app = new AppController(data);
     });
 });
-
-function sendMsgToContent(op) {
-    chrome.storage.sync.get(null, function(d) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {'op': op, 'data': d});
-        });
-    });
-}
-
-function saveChangesAtStorage() {
-    if (mode == 0){
-        chrome.storage.sync.set({'keywordList': Array.from(keywordList)}, function() {
-            console.log("Changes saved at keywordList");
-        });
-    } else {
-        chrome.storage.sync.set({'userList': Array.from(userList)}, function() {
-            console.log("Changes saved at userList");
-        });
-    }
-}
